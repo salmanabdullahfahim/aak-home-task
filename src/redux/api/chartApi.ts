36c42a66ff292e8cf0bb98f3ec9a069c59605783
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-12-24 20:26:59 +0600

    Refactor chartApi: Introduce TResult type for structured response and update getChartData query to utilize typed results

--- src/redux/api/chartApi.ts
+++ src/redux/api/chartApi.ts
@@ -1,15 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { TResult } from "../../types";
 
 export const chartApi = createApi({
   reducerPath: "chartApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://django-dev.aakscience.com/candidate_test/fronted",
   }),
-  endpoints: (builder) => {
-    getChartData: builder.query({
+  endpoints: (builder) => ({
+    getChartData: builder.query<any, void>({
       query: () => "",
       transformResponse: (response: any) => {
-        const result = [];
+        const result: TResult[] = [];
         response[0]?.["2024"].forEach((month: any) => {
           Object.values(month).forEach((entries: any) => {
             entries.forEach((entry: any) => {
@@ -20,8 +21,8 @@ export const chartApi = createApi({
         });
         return result;
       },
-    });
-  },
+    }),
+  }),
 });
 
 export const { useGetChartDataQuery } = chartApi;
---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export type TResult = {
+  date: string;
+  value: number;
+};
