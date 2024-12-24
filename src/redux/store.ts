813c5bd96587dc8852987d3a790e2c22c8cf8698
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-12-24 20:16:12 +0600

    Adding chartApi middleware for improved API handling

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,6 +5,8 @@ export const store = configureStore({
   reducer: {
     [chartApi.reducerPath]: chartApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(chartApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
